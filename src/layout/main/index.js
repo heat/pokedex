235eb038905eea46a1466e622a438503888d3a8f
Onezino Gabriel <zinogabriel@gmail.com>
2018-03-02 00:45:54 -0300

    cria componente pokedex

--- src/layout/main/index.js
+++ src/layout/main/index.js
@@ -4,22 +4,26 @@ import {
     Route,
     Link
   } from 'react-router-dom';
+import {
+    Navbar,
+    Nav,
+    NavItem
+} from 'react-bootstrap';
 import logo from '../../logo.svg';
-
+import './main.css';
 class App extends Component {
   render() {
     return (
-    <div className="App  container">
-        <div>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/topics">Topics</Link></li>
-            </ul>
-
-            <hr />
-
-        </div>
+    <div className="pokedex container">
+        <header>
+            <h1>POKEDEX V1.2</h1>
+        </header>
+        <Navbar>
+            <Nav>
+                <NavItem eventKey={1} href="/">POKEDEX</NavItem>
+                <NavItem eventKey={2} href="/about">CAPTURAR</NavItem>
+            </Nav>
+        </Navbar>
         {this.props.children}
     </div>
     );
